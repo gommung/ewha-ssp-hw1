ce676821a5578825939cb04b7733cd1e510005b8
gommung <wkao86@naver.com>
2016-03-23 14:02:28 +0900

    Update ewha_ssp_hw1_1.cpp

--- ewha_ssp_hw1_1.cpp
+++ ewha_ssp_hw1_1.cpp
@@ -5,20 +5,35 @@
  * GitHub Setup Homework 
  * Instructor: Prof. HyungJune Lee
  *
- * Coded by Firstname Lastname
- * Mar xx, 2015
+ * Coded by chaeryung, HUH
+ * Mar 23, 2016
  *
  */
+#include "LED_test.h"
+int Front_LED =10;
+int Back_LED =9;
+int i;
+
 
 // Add #define pre-processor below
 
  
 void setup() {
-  //Add your code of Lab Assignment #1
+ pinMode(Front_LED,OUTPUT);
+pinMode(Back_LED,OUTPUT);
 
 }
 
 void loop() {
-  //Add your code of Lab Assignment #1  
+  for(i=0;i<=225;i++){
+	analogWrite(Front_LED,i);
+	analogWrite(Back_LED,255-i);
+    delay(11);
+}
+for(i=0;i<=225;i++){
+	analogWrite(Front_LED,255-i);
+	analogWrite(Back_LED,i);
+    delay(11);
+}
 
 }
